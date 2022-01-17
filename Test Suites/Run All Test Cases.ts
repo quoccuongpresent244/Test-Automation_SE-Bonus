6d9140669803a7c3e2faad4fc3b4987d36d3a1ed
quoccuongpresent <nguyenduyanhquoc1st@gmail.com>
2022-01-17 21:44:36 +0700

    Iteration 2

--- Test Suites/Run All Test Cases.ts
+++ Test Suites/Run All Test Cases.ts
@@ -5,7 +5,7 @@
    <tag></tag>
    <isRerun>false</isRerun>
    <mailRecipient></mailRecipient>
-   <numberOfRerun>3</numberOfRerun>
+   <numberOfRerun>0</numberOfRerun>
    <pageLoadTimeout>1</pageLoadTimeout>
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
@@ -14,85 +14,85 @@
    <testCaseLink>
       <guid>169f7588-4e2c-405d-92a7-e49eef224ff7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Create new private space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>4ae4afe1-7ae2-4ae8-89e9-d6f270aa3556</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Create new private room in a space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>f232f567-97ae-42cb-b384-0f430be342d7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Remove a room in a space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>7f083b7a-a1e3-4284-8ab1-8e2af4bd6173</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Create new public room in a space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>b1d5c2bd-c153-49d6-b8b1-ff0dcb34adce</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Send direct message to a person</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>ad7fdc7f-da33-4323-b225-98f333adfb32</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Delete a message in chat</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>2445fa36-c161-4384-9dcb-b0ee812caafc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Check direct message from a person</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>75dfab6f-679f-4529-a6ee-e30d57ca0846</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Send emoji in a direct message</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>8b77c936-74ef-49ac-9b02-5db55bf0347f</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Invite a user to my private space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>a6e9edb4-81dd-44d7-9b70-db392468585b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Receive and accept invite to join a space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>bb4e1f68-e28d-4afa-902f-993a3a54b2dc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Kick a member from my space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>e21d7f23-08d7-447d-bea7-ba3bc8c68fc7</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Create new public space</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>63e21ecf-f11e-4ba6-9021-a2841563d5f6</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Set up for retest 1</testCaseId>
    </testCaseLink>
    <testCaseLink>
       <guid>3486f41d-69f6-4d00-aa9e-2ff4d4f5a2dc</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Set up for retest 2</testCaseId>
    </testCaseLink>
    <testCaseLink>
@@ -130,5 +130,65 @@
       <isReuseDriver>false</isReuseDriver>
       <isRun>true</isRun>
       <testCaseId>Test Cases/50Testcase/Leave Room</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>d4e60752-d3d3-43b5-ac77-76bfa9f9c1ba</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien1</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b8058f0c-232f-4b9b-a312-163c2558742e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien2</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>94de6630-0de4-4b63-b07e-2cf8c2f5fbd6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>5a60f899-9c46-42a1-a931-9ff642c7a897</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien4</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f445cefb-1960-4751-a37f-6a91ccd0033c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien5</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>06083338-a897-429f-a606-1edfb1a00f3c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien6</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>6236d2ed-d322-4f17-9a08-dad4d83e260b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien7</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b1f32ffb-8c91-4bf7-afa5-565935a9c49f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien8</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>80fb4f0e-5005-4870-b506-377f24490e08</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien9</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>33cd5282-5027-4870-9474-a948204e5b04</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/50Testcase/DienTest/TestDien10</testCaseId>
    </testCaseLink>
 </TestSuiteEntity>
